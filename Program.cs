6a94ee341dc77f3f7ce4b6eff52c7168690bcb0a
tao.unit <tao.unit@gmail.com>
2024-12-05 05:49:03 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+            Console.WriteLine("Hello Ab");
+            Console.WriteLine("How do you feel in one word?");
+            string howAreYouDoingReply = Console.ReadLine();
+            Console.WriteLine("Oh I see! You feel " + howAreYouDoingReply + "!");
+            Console.WriteLine("have a nice day Ab!");
+            Console.WriteLine("And how old are you?");
+            string howOldAreYouReply = Console.ReadLine();
+            Console.WriteLine("Oh I see! You are still a very healthy " + howOldAreYouReply + " year old guy!");
+            Console.WriteLine("Type bye to say bye!");
+            Console.ReadLine();
+
+
